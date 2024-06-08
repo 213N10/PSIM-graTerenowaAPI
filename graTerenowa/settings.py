c7c843bcb1015658463ee01591b2fe770871649b
Janek <75185339+213N10@users.noreply.github.com>
2024-06-08 15:21:31 +0200

    maybe glitch.com gonna work

--- graTerenowa/settings.py
+++ graTerenowa/settings.py
@@ -15,8 +15,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+#SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 #'django-insecure-(%%bqd4+u$m=%wvgkb-o_^0o$+(#6%a+$uezw)w=6c7j%ze_fw'
 
